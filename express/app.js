e8d9f36a4b716b7861faecfacd3207ff8658fa7f
Hyun Kyung Han <hhk@ewhain.net>
2022-05-28 21:42:31 +0900

    Add files via upload

--- express/app.js
+++ express/app.js
@@ -1,20 +1,36 @@
-//express는 middleware들이 chain으로 연결된 형태.
-//1.req 실습하기
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
 
-import express from 'express'  //express 가져오기
-const app = express();   //app 만들기
+// http://expressjs.com/en/resources/middleware/morgan.html
+const app = express();
 
-app.get('/sky/:id', (req, res, next) => {
-    //console.log(req.path);   //path(경로)에 대한 정보 확인 가능
-    //console.log(req.headers);   //header 정보 확인 가능
-    console.log(req.params);   //param형태 정보 확인 가능 (http://localhost:8080/sky/hk)
-    console.log(req.params.id);  
+// cookie-parser
+// morgan
+// cors
+// helmet
 
-    console.log(req.query);    //query에 대한 정보 확인 가능 (http://localhost:8080/sky/hk/?keyword=bts)
-    console.log(req.query.keyword);
-    res.send('hi');  //클라이언트는 입력이 올때까지 계속 로딩하고 있으므로 클라이언트에게 항상 data를 보내줘야 한다.
+// cookie-parser
+// key: Cookie
+// value: yummy_cookie=choco; tasty_cookie=strawberry
+
+const corsOptions = {
+  origin: ['http://localhost:3000'],
+  optionsSuccessStatus: 200, // for options request
+  credentials: true, // Access-Control-Allow-Credentials: true
+};
+
+app.use(cookieParser()); // http://expressjs.com/en/resources/middleware/cookie-parser.html
+app.use(morgan('common')); // http://expressjs.com/en/resources/middleware/morgan.html
+app.use(cors(corsOptions));
+app.use(helmet()); // https://github.com/helmetjs/helmet
+
+app.get('/', (req, res) => {
+  console.log(req.cookies); // it will be undefined without cookie-parser
+  console.log(req.cookies.yummy_cookie);
+  res.send('Welsome!');
 });
-app.listen(8080);      //8080이라는 포트에서 listen (url창에 localhost:8080 입력하기)
-//여기까지만 진행해도 server가 생성됨
 
-
+app.listen(8080);
