b90099a9054ab30aca9f6a0ed31b25d2a2175180
Hyun Kyung Han <hhk@ewhain.net>
2022-06-26 03:13:31 +0900

    Add files via upload

---
+++ 이화이언0626과제/db/database.js
@@ -0,0 +1,18 @@
+import { config } from '../config.js';
+import mysql from 'mysql2';
+import SQ from 'sequelize';
+
+const {host, user, database, password } = config.db;
+export const sequelize = new SQ.Sequelize(database, user, password, {
+    host, 
+    dialect: 'mysql', /*기본값이 mysql이라 생략가능.*/
+});
+
+const pool = mysql.createPool({
+    host: config.db.host,
+    user: config.db.user,
+    database: config.db.database,
+    password: config.db.password,
+});
+
+export const db = pool.promise();
